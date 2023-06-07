2982b6c3c9a40dd4c15d3125042daafeb2499a93
douglasojesus <102335062+douglasojesus@users.noreply.github.com>
2023-06-07 19:36:31 -0300

    :bug: fix: Correção da escolha dos decodificadores

--- protótipo/MainCoffeeMachine/Decoders.v
+++ protótipo/MainCoffeeMachine/Decoders.v
@@ -23,14 +23,14 @@ module Decoders(CLK, S0, S1, S2, S3, SR, SP, SN, VL, M, a, b, c, d, e, f, g, h);
 	
 	Contador(CLK, saida1Contador, saida2Contador);
 	
-	InterfaceS0 (saida1Contador, saida2Contador, A1, B1, C1, D1, E1, F1, G1);
-	InterfaceS1 (saida1Contador, saida2Contador, A2, B2, C2, D2, E2, F2, G2);
-	InterfaceS2 (saida1Contador, saida2Contador, A3, B3, C3, D3, E3, F3, G3);
-	InterfaceS3 (saida1Contador, saida2Contador, A4, B4, C4, D4, E4, F4, G4);
-	InterfaceESR (saida1Contador, saida2Contador, A5, B5, C5, D5, E5, F5, G5);
-	InterfaceESP (saida1Contador, saida2Contador, A6, B6, C6, D6, E6, F6, G6);
-	InterfaceESN (saida1Contador, saida2Contador, A7, B7, C7, D7, E7, F7, G7);
-	InterfaceERDI (saida1Contador, saida2Contador, A8, B8, C8, D8, E8, F8, G8);
+	InterfaceS0 (saida1Contador, saida2Contador, A1, B1, C1, D1, E1, F1, G1, S0, S1, S2, S3, SR, SP, SN, VL);
+	InterfaceS1 (saida1Contador, saida2Contador, A2, B2, C2, D2, E2, F2, G2, S0, S1, S2, S3, SR, SP, SN, VL);
+	InterfaceS2 (saida1Contador, saida2Contador, A3, B3, C3, D3, E3, F3, G3, S0, S1, S2, S3, SR, SP, SN, VL);
+	InterfaceS3 (saida1Contador, saida2Contador, A4, B4, C4, D4, E4, F4, G4, S0, S1, S2, S3, SR, SP, SN, VL);
+	InterfaceESR (saida1Contador, saida2Contador, A5, B5, C5, D5, E5, F5, G5, S0, S1, S2, S3, SR, SP, SN, VL);
+	InterfaceESP (saida1Contador, saida2Contador, A6, B6, C6, D6, E6, F6, G6, S0, S1, S2, S3, SR, SP, SN, VL);
+	InterfaceESN (saida1Contador, saida2Contador, A7, B7, C7, D7, E7, F7, G7, S0, S1, S2, S3, SR, SP, SN, VL);
+	InterfaceERDI (saida1Contador, saida2Contador, A8, B8, C8, D8, E8, F8, G8, S0, S1, S2, S3, SR, SP, SN, VL);
 	
 	or (a, A1, A2, A3, A4, A5, A6, A7, A8);
 	or (b, B1, B2, B3, B4, B5, B6, B7, B8);
@@ -40,6 +40,5 @@ module Decoders(CLK, S0, S1, S2, S3, SR, SP, SN, VL, M, a, b, c, d, e, f, g, h);
 	or (f, F1, F2, F3, F4, F5, F6, F7, F8);
 	or (g, G1, G2, G3, G4, G5, G6, G7, G8);
 
-	
 	
 endmodule
--- protótipo/MainCoffeeMachine/InterfaceESR.v
+++ protótipo/MainCoffeeMachine/InterfaceESR.v
@@ -1,8 +1,7 @@
-module InterfaceESR (saida1Contador, saida2Contador, a, b, c, d, e, f, g);
+module InterfaceESR (saida1Contador, saida2Contador, a, b, c, d, e, f, g, S0, S1, S2, S3, SR, SP, SN, VL);
 	//ERSR: erro do sensor SR
-	input saida1Contador, saida2Contador;
+	input saida1Contador, saida2Contador, S0, S1, S2, S3, SR, SP, SN, VL;
 	output a, b, c, d, e, f, g;
-
 
 	wire saida1a, saida2a, saida3a, saida4a,
 	saida1b, saida2b, saida3b, saida4b,
@@ -13,11 +12,14 @@ module InterfaceESR (saida1Contador, saida2Contador, a, b, c, d, e, f, g);
 	saida1g, saida2g, saida3g, saida4g,
 	saida1h, saida2h, saida3h, saida4h;
 	
+	wire enable;
+	and (enable, ~S0, ~S1, ~S2, ~S3, SR, ~SP, ~SN, ~VL);
+	
 	//a
 	//como o segmento a é ativado 3 vezes nas letras CE0, ele fica nessa configuração no multiplexador
-	and(saida1a, ~saida1Contador, ~saida2Contador, 1);
+	and(saida1a, ~saida1Contador, ~saida2Contador, 1, enable);
 	and(saida2a, ~saida1Contador, saida2Contador, 0);
-	and(saida3a, saida1Contador, ~saida2Contador, 1);
+	and(saida3a, saida1Contador, ~saida2Contador, 1, enable);
 	and(saida4a, saida1Contador, saida2Contador, 0);
 	or(a, saida1a, saida2a, saida3a, saida4a);
 	
@@ -31,36 +33,36 @@ module InterfaceESR (saida1Contador, saida2Contador, a, b, c, d, e, f, g);
 	//c
 	and(saida1c, ~saida1Contador, ~saida2Contador, 0);
 	and(saida2c, ~saida1Contador, saida2Contador, 0);
-	and(saida3c, saida1Contador, ~saida2Contador, 1);
+	and(saida3c, saida1Contador, ~saida2Contador, 1, enable);
 	and(saida4c, saida1Contador, saida2Contador, 0);
 	or(c, saida1c, saida2c, saida3c, saida4c);
 	
 	//d
-	and(saida1d, ~saida1Contador, ~saida2Contador, 1);
+	and(saida1d, ~saida1Contador, ~saida2Contador, 1, enable);
 	and(saida2d, ~saida1Contador, saida2Contador, 0);
-	and(saida3d, saida1Contador, ~saida2Contador, 1);
+	and(saida3d, saida1Contador, ~saida2Contador, 1, enable);
 	and(saida4d, saida1Contador, saida2Contador, 0);
 	or(d, saida1d, saida2d, saida3d, saida4d);
 	
 	//e
-	and(saida1e, ~saida1Contador, ~saida2Contador, 1);
-	and(saida2e, ~saida1Contador, saida2Contador, 1);
+	and(saida1e, ~saida1Contador, ~saida2Contador, 1, enable);
+	and(saida2e, ~saida1Contador, saida2Contador, 1, enable);
 	and(saida3e, saida1Contador, ~saida2Contador, 0);
-	and(saida4e, saida1Contador, saida2Contador, 1);
+	and(saida4e, saida1Contador, saida2Contador, 1, enable);
 	or(e, saida1e, saida2e, saida3e, saida4e);
 	
 	//f
-	and(saida1f, ~saida1Contador, ~saida2Contador, 1);
+	and(saida1f, ~saida1Contador, ~saida2Contador, 1, enable);
 	and(saida2f, ~saida1Contador, saida2Contador, 0);
-	and(saida3f, saida1Contador, ~saida2Contador, 1);
+	and(saida3f, saida1Contador, ~saida2Contador, 1, enable);
 	and(saida4f, saida1Contador, saida2Contador, 0);
 	or(f, saida1f, saida2f, saida3f, saida4f);
 	
 	//g
-	and(saida1g, ~saida1Contador, ~saida2Contador, 1);
-	and(saida2g, ~saida1Contador, saida2Contador, 1);
-	and(saida3g, saida1Contador, ~saida2Contador, 1);
-	and(saida4g, saida1Contador, saida2Contador, 1);
+	and(saida1g, ~saida1Contador, ~saida2Contador, 1, enable);
+	and(saida2g, ~saida1Contador, saida2Contador, 1, enable);
+	and(saida3g, saida1Contador, ~saida2Contador, 1, enable);
+	and(saida4g, saida1Contador, saida2Contador, 1, enable);
 	or(g, saida1g, saida2g, saida3g, saida4g);
 
 
